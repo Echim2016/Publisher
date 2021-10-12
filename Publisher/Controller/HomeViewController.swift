71c3777065e8756642510d938b44c06bc59080cd
Yi-Chin Hsu <ychim26@gmail.com>
2021-10-12 09:54:14 +0800

    <feat>  setUpNavigationbar() built

--- Publisher/Controller/HomeViewController.swift
+++ Publisher/Controller/HomeViewController.swift
@@ -12,8 +12,27 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupNavigationBar()
+    }
+
+}
+
+extension HomeViewController {
+    
+    func setupNavigationBar() {
+        
+        let titleLabel = UILabel()
+        titleLabel.text = "Publisher"
+        titleLabel.textColor = .white
+        titleLabel.font = UIFont(name: "PingFangTC-Semibold", size: 20)
+        self.navigationItem.titleView = titleLabel
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor.bluePurple
+        navigationController?.navigationBar.standardAppearance = appearance;
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
     }
     
-
+    
 }
