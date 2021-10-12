f57185ddf91b21e304a8390e1d070baa0e548b61
Yi-Chin Hsu <ychim26@gmail.com>
2021-10-12 10:35:25 +0800

    <feat> Publish Page and Article struct built

--- Publisher/Controller/PublishViewController.swift
+++ Publisher/Controller/PublishViewController.swift
@@ -6,9 +6,35 @@
 //
 
 import UIKit
+import Firebase
+
 
 class PublishViewController: UIViewController {
 
+    
+    @IBOutlet weak var titleTextField: UITextField! {
+        didSet {
+            titleTextField.delegate = self
+            titleTextField.accessibilityLabel = "title"
+        }
+    }
+    
+    @IBOutlet weak var categoryTextField: UITextField! {
+        didSet {
+            categoryTextField.delegate = self
+            categoryTextField.accessibilityLabel = "category"
+        }
+    }
+    
+    @IBOutlet weak var contentTextField: UITextField! {
+        didSet {
+            contentTextField.delegate = self
+            contentTextField.accessibilityLabel = "content"
+        }
+    }
+    
+    var article = Article.init(id: "", title: "", content: "", tag: "", author: "echim", createdTime: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +42,50 @@ class PublishViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func pressPublish(_ sender: Any) {
+        
+        
     }
-    */
+    
+}
 
+// MARK: - Publish Action -
+extension PublishViewController {
+    
+    func publsihArticle() {
+        
+    }
+    
+}
+
+
+
+// MARK: - TextField -
+extension PublishViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        guard let text = textField.text,
+              !text.isEmpty else {
+            print("empty input")
+            return
+        }
+        
+        switch textField.accessibilityLabel {
+        case "title":
+            article.title = text
+        case "content":
+            article.content = text
+        case "tag":
+            article.tag = text
+        default:
+            break
+        }
+    }
 }
---
+++ Publisher/Model/Article.swift
@@ -0,0 +1,19 @@
+//
+//  Article.swift
+//  Publisher
+//
+//  Created by Yi-Chin Hsu on 2021/10/12.
+//
+
+import Foundation
+
+struct Article {
+    
+    var id: String
+    var title: String
+    var content: String
+    var tag: String
+    var author: String
+    var createdTime: NSDate?
+    
+}
