1ab6da472f8018859c815a55f73076d1029fdb3b
Yi-Chin Hsu <ychim26@gmail.com>
2021-10-12 10:06:32 +0800

    <view> article cell built

---
+++ Publisher/View/ArticleCell.swift
@@ -0,0 +1,19 @@
+//
+//  ArticleCell.swift
+//  Publisher
+//
+//  Created by Yi-Chin Hsu on 2021/10/12.
+//
+
+import UIKit
+
+class ArticleCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var categoryButton: UIButton!
+    @IBOutlet weak var createdTimeLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    
+    
+}
