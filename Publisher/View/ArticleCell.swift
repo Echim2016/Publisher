2286a02a7bdf1d43d3355d4ddf1b5d9969279f03
Yi-Chin Hsu <ychim26@gmail.com>
2021-10-12 16:00:00 +0800

    <fix> category label improved

--- Publisher/View/ArticleCell.swift
+++ Publisher/View/ArticleCell.swift
@@ -11,11 +11,7 @@ class ArticleCell: UITableViewCell {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var authorLabel: UILabel!
-    @IBOutlet weak var categoryButton: UIButton! {
-        didSet {
-            categoryButton.isUserInteractionEnabled = false
-        }
-    }
+    @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var createdTimeLabel: UILabel!
     @IBOutlet weak var contentLabel: UILabel!
     
@@ -24,7 +20,9 @@ class ArticleCell: UITableViewCell {
         titleLabel.text = article.title
         authorLabel.text = article.author.name
         contentLabel.text = article.content
-        categoryButton.titleLabel?.text = article.category
+        categoryLabel.text = article.category
+        categoryLabel?.layer.cornerRadius = 5
+        categoryLabel?.layer.masksToBounds = true
         createdTimeLabel.text = article.createdTime.getFormattedDate(format: "yyyy-MM-dd HH:mm:ss")
     }
     
